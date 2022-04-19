cc5239ee88096028dd7ad753616bada56a25f52e
amar-ox <a_abane@hotmail.fr>
2022-04-19 11:34:01 -0400

    fix: dev build script errors

--- local/run.sh
+++ local/run.sh
@@ -13,18 +13,18 @@ export CONTROLLER_VOL=$1
 
 # Start persistence containers
 docker-compose -f docker-compose.yml up -d mysql mongo activemq neo4j
-sleep 30
+sleep 15
 
-mvn -f ../pom.xml clean install
+mvn -f ../pom.xml clean package -Dmaven.test.skip=true
 
 # account
-java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../account-microservice/target/account-microservice-fat.jar -cluster -ha -conf ../account-microservice/src/config/local.json > multiverse.log 2>&1 &
+java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../account-microservice/target/account-microservice-fat.jar -cluster -ha -conf ../account-microservice/src/config/local.json &
 sleep 1
 # telemetry
-java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../telemetry-microservice/target/telemetry-microservice-fat.jar -cluster -ha -conf ../telemetry-microservice/src/config/local.json > multiverse.log 2>&1 &
+java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../telemetry-microservice/target/telemetry-microservice-fat.jar -cluster -ha -conf ../telemetry-microservice/src/config/local.json &
 sleep 1
 # notification
-java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../notification-microservice/target/notification-microservice-fat.jar -cluster -ha -conf ../notification-microservice/src/config/local.json > multiverse.log &
+java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../notification-microservice/target/notification-microservice-fat.jar -cluster -ha -conf ../notification-microservice/src/config/local.json &
 sleep 1
 # topology
 java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../topology-microservice/target/topology-microservice-fat.jar -cluster -ha -conf ../topology-microservice/src/config/local.json &
@@ -33,16 +33,16 @@ sleep 1
 java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../digitaltwin-microservice/target/digitaltwin-microservice-fat.jar -cluster -ha -conf ../digitaltwin-microservice/src/config/local.json &
 sleep 1
 # ndnet
-java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../ndnet-microservice/target/ndnet-microservice-fat.jar -cluster -ha -conf ../ndnet-microservice/src/config/local.json multiverse.log &
+java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../ndnet-microservice/target/ndnet-microservice-fat.jar -cluster -ha -conf ../ndnet-microservice/src/config/local.json &
 sleep 1
 # qconnection
-java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../qconnection-microservice/target/qconnection-microservice-fat.jar -cluster -ha -conf ../qconnection-microservice/src/config/local.json multiverse.log &
+java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../qconnection-microservice/target/qconnection-microservice-fat.jar -cluster -ha -conf ../qconnection-microservice/src/config/local.json &
 sleep 1
 # qnet
-java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../qnet-microservice/target/qnet-microservice-fat.jar -cluster -ha -conf ../qnet-microservice/src/config/local.json multiverse.log &
+java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../qnet-microservice/target/qnet-microservice-fat.jar -cluster -ha -conf ../qnet-microservice/src/config/local.json &
 sleep 1
 # ipnet
-java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../ipnet-microservice/target/ipnet-microservice-fat.jar -cluster -ha -conf ../ipnet-microservice/src/config/local.json multiverse.log &
+java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../ipnet-microservice/target/ipnet-microservice-fat.jar -cluster -ha -conf ../ipnet-microservice/src/config/local.json &
 sleep 1
 # api-gw
-java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../api-gateway/target/api-gateway-fat.jar -cluster -ha -conf ../api-gateway/src/config/local.json > multiverse.log 2>&1 multiverse.log &
+java -Djava.net.preferIPv4Stack=true -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar ../api-gateway/target/api-gateway-fat.jar -cluster -ha -conf ../api-gateway/src/config/local.json &
